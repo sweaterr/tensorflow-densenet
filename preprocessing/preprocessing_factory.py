ee2bb807b76a1a69eff3262b50ebcd908452d5f5
jungkyu-lee <jungkyu.lee@navercorp.com>
2017-09-07 12:00:10 +0900

    cifar preprocessiong

--- preprocessing/preprocessing_factory.py
+++ preprocessing/preprocessing_factory.py
@@ -22,6 +22,7 @@ import tensorflow as tf
 
 from preprocessing import densenet_preprocessing
 from preprocessing import basic_preprocessing
+from preprocessing import cifarnet_preprocessing
 
 slim = tf.contrib.slim
 
@@ -46,7 +47,8 @@ def get_preprocessing(name, is_training=False):
       'densenet121': densenet_preprocessing,
       'densenet161': densenet_preprocessing,
       'densenet169': densenet_preprocessing,
-      'basic': basic_preprocessing
+      'basic': basic_preprocessing,
+      'cifar10': cifarnet_preprocessing
   }
 
   if name not in preprocessing_fn_map:
