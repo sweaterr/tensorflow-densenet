269ee8348ab2a9a57bd7a3036ed032030646a540
jungkyu-lee <jungkyu.lee@navercorp.com>
2017-09-20 16:50:18 +0900

    convert flowers600 to tfrecord

--- datasets/download_and_convert_flowers600.py
+++ datasets/download_and_convert_flowers600.py
@@ -137,8 +137,8 @@ def _convert_dataset(split_name, filenames, class_names_to_ids, dataset_dir):
             # Read the filename:
             image_data = tf.gfile.FastGFile(filenames[i], 'rb').read()
             height, width = image_reader.read_image_dims(sess, image_data)
-            image_reader.decode_jpeg(sess, image_data)
-
+            if height == 501 and width == 753:
+              print(filenames[i])
             class_name = os.path.basename(os.path.dirname(filenames[i]))
             class_id = class_names_to_ids[class_name]
 
